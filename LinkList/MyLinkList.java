a90fbbec323865821983d796a80030d39dbb9434
yourenA <1551608967@qq.com>
2020-07-02 19:01:17 +0800

    修改

--- LinkList/MyLinkList.java
+++ LinkList/MyLinkList.java
@@ -17,30 +17,38 @@ public class MyLinkList {
     public MyLinkList(){};
     //初始化链表
     public void initListNode(){
-        if(null != head) return;
+        if(null != head) {
+            return;
+        }
         head = new ListNode();
-        if(null == head)
+        if(null == head){
             throw new GetHeapMemoryException("获取内存构建对象失败！");
+        }
         return ;
     }
     //判断链表是否初始化
     public void isInit(){
-        if(null == head)
+        if(null == head) {
             throw new InitLinkListException("链表未初始化！");
+        }
         return ;
     }
     //判断链表是否为空
     public boolean isEmpty(){
-        if(null != head.next) return false;
-        else return true;
+        if(null != head.next) {
+            return false;
+        }
+        else {
+            return true;
+        }
     }
     //根据pos返回偏移后的节点引用
     private ListNode getListNode(int pos)throws IllegalPosException{
-        if(pos < 0 || pos > listSize)
+        if(pos < 0 || pos > listSize){
             throw new IllegalPosException("输入的pos不合法！");
+        }
         ListNode node = head;
-        for(int i = 0; i < pos; i++)
-        {
+        for(int i = 0; i < pos; i++) {
             node =node.next;
         }
         return node;
@@ -64,10 +72,11 @@ public class MyLinkList {
     }
     //查找num并返回其引用，若有重复返回第一个节点的引用
     private ListNode selectLinkNode(int num){
-        if(isEmpty()) return null;
+        if(isEmpty()){
+            return null;
+        }
         ListNode p = head.next;
-        while(p.num!=num)
-        {
+        while(p.num!=num){
             p=p.next;
         }
         return p;
@@ -86,7 +95,9 @@ public class MyLinkList {
         boolean res = true;
         isInit();
         ListNode p = selectLinkNode(num);
-        if(null == p) res = false;
+        if(null == p){
+            res = false;
+        }
         p.next = p.next.next;
         listSize -= 1;
         return res;
@@ -95,7 +106,9 @@ public class MyLinkList {
     public boolean isNumLinkList(int num){
         boolean res = false;
         isInit();
-        if(null!= selectLinkNode(num)) res = true;
+        if(null!= selectLinkNode(num)){
+            res = true;
+        }
         return res;
     }
     //链表头插
@@ -105,8 +118,9 @@ public class MyLinkList {
     }
     //链表头删
     public void pop_front()throws IllegalPosException{
-        if(!isEmpty())
+        if(!isEmpty()){
             deletePosListNode(0);
+        }
         return;
     }
     // 链表拷贝
